6f75c19aebc0f357af3b72b3f9ce19a242cdc97d
premiumsupportofficial <152652524+premiumsupportofficial@users.noreply.github.com>
2023-12-07 13:01:42 +0530

    Update theme.js
    Speed up to 100ms

--- js/theme.js
+++ js/theme.js
@@ -72,7 +72,7 @@ $("body").scrollspy({
 // Wait for window load
 $(window).load(function () {
   // Animate loader off screen
-  $(".page-loader").fadeOut(900);
+  $(".page-loader").fadeOut(100);
 });
 
 ////////////////////////////////////////////////////
